256f96ce7d0d43bedcee5ce0e559fdeec345ebf8
Santigodev <shilton.santiago@gmail.com>
2025-02-25 20:15:31 -0300

    Criando o projeto

---
+++ api-acesso-ia/Services/LoginService.cs
@@ -0,0 +1,50 @@
+﻿using api_acesso_ia.Models;
+using api_acesso_ia.Repositories.Interfaces;
+using api_acesso_ia.Services.Interfaces;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace api_acesso_ia.Services
+{
+    public class LoginService : ILoginService
+    {
+        private readonly ILoginRepository _loginRepository;
+
+        public LoginService(ILoginRepository loginRepository)
+        {
+            _loginRepository = loginRepository;
+        }
+
+        public async Task<LoginUsuario> AutenticarService(string login, string senha)
+        {
+            var senhaHash = CriptografarSenha(senha);
+            return await _loginRepository.Autenticar(login, senhaHash);
+        }
+
+        public async Task<LoginUsuario> CadastrarService(LoginUsuario dados)
+        {
+            return await _loginRepository.Cadastrar(dados);
+        }
+
+        public async Task<bool> CpfJaCadastradoService(string cpf)
+        {
+            var possui = await _loginRepository.CpfJaCadastrado(cpf);
+
+            if (possui)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string CriptografarSenha(string senha)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(senha);
+                var hash = sha256.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
